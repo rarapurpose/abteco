c166a40d882fe9f955a7e04ec4485bcf75c8e65f
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-26 18:16:37 +0900

    comment set up

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,7 @@ class Post < ApplicationRecord
   validates  :title,      presence: true
   validates  :detail,     presence: true
   validates  :image,      presence: true
+
+  has_many   :comments, dependent: :destroy
   
 end
